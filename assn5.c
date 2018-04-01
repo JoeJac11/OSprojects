ed4a2e1492fa9569a72c3fd9c2f23b79e6c008e2
Joe Jacobson <joe.c.jacobson@gmail.com>
2018-03-31 23:14:02 -0600

    made all the files play nice together

--- assn5.c
+++ assn5.c
@@ -1,5 +1,14 @@
+/*I promise that this source code file has, in its entirety, been
+written by myself or a member of my group and by no other person or
+persons. If at any time an exact copy of this source code is found to
+be used by another person outside my group in this term, I understand
+that all members of my group and the members of the group that
+submitted the copy will receive a zero on this assignment.
+*/
+//Kris Olson
 #include <stdio.h>
 #include <stdlib.h>
+#include "assn5.h"
 
 #define MAX_PROCS (100)
 #define TIME_Q (100)
@@ -45,110 +54,6 @@ void FCFS(int *arr, int *burst, int count) {
            (float) wait / count);
 }
 
-void SJF(int *arr, int *burst, int count) {
-    int ready[MAX_PROCS], start[MAX_PROCS], end[MAX_PROCS];
-    int clock = 0;
-    int ta = 0, wait = 0, resp = 0;
-    int cur = 0, next = 0, insert = 0, inQ = 0;
-
-    for (int i = 0; i < count; ++i) {
-        if (cur == insert) {
-            ready[insert++] = next;
-            clock = arr[next];
-            ++inQ;
-            ++next;
-        }
-        start[ready[cur]] = clock;
-        clock += burst[ready[cur]];
-        end[ready[cur]] = clock;
-        inQ;
-        ta += end[ready[cur]] - arr[ready[cur]];
-        wait += start[ready[cur]] - arr[ready[cur]];
-        resp += start[ready[cur]] - arr[ready[cur]];
-        while (next < count && arr[next] < clock){
-            ready[insert++] = next++;
-            ++inQ;
-            for(int i = cur + 1; i < inQ + 1; ++i) {
-                for (int j = i + 1; j < inQ; ++j){
-                    if(burst[ready[i]] > burst[ready[j]]) {
-                        int temp = ready[i];
-                        ready[i] = ready[j];
-                        ready[j] = temp;
-                    }
-                }
-            }
-        }
-        ++cur;
-    }
-    printf("Shortest Job First\n");
-
-    printf("Avg Resp: %.2f  Avg TA: %.2f  Avg Wait: %.2f\n\n", (float) resp / count, (float) ta / count, (float) wait / count);
-}
-
-void SRTF(int *arr, int *burst, int count) {
-    int ready[MAX_PROCS], start[MAX_PROCS], end[MAX_PROCS], remain[MAX_PROCS];
-    int clock = 0;
-    int ta = 0, wait = 0, resp = 0;
-    int next = 1, cur = 0, insert = 0;
-    
-    for (int i = 0; i < count; ++i)  {
-        if (cur == 0) {//first run
-            ready[insert++] = cur;
-            remain[cur] = burst[cur];
-            clock = arr[cur];
-            start[cur] = clock;
-        }
-        else if (cur == insert){//ready queue is empty
-            ready[insert++] = next;
-            remain[next] = burst[next];
-            clock = arr[next];
-            start[next] = -1;
-        }
-
-        if (start[ready[cur]] < 0) start[ready[cur]] = clock;
-
-        if (clock + remain[ready[cur]] < arr[next]){//job is done
-            end[ready[cur]] = clock + remain[ready[cur]];
-printf("start %d job %d end %d\n",start[ready[cur]], ready[cur], end[ready[cur]]);
-            remain[ready[cur]] = 0;
-            ready[insert++] = next;
-            remain[next] = burst[next];
-            clock = arr[next];
-            start[next] = -1;
-        }
-        else {//has time left
-            remain[ready[cur]] -= arr[next] - clock;
-            ready[insert++] = next;
-            remain[next] = burst[next];
-            start[next] = -1;
-            clock = arr[next];
-            ready[insert++] = ready[cur];
-        }
-        ++cur;
-        ++next;
-
-        for(int i = cur; i < insert; ++i) {//sort ready queue
-            for (int j = i + 1; j < insert; ++j){
-                if(remain[ready[i]] > remain[ready[j]]) {
-                    int temp = ready[i];
-                    ready[i] = ready[j];
-                    ready[j] = temp;
-                }
-            }
-        }
-    }
-        
- 
-    for(int i = 0; i < count; ++i){
-        int curTa = end[i] - arr[i];
-        ta += curTa;
-        wait += end[i] - burst[i] - arr[i];
-        resp += start[i] - arr[i];
-    }
-
-    printf("Shortest Remaining Time First\n");
-    printf("Avg Resp: %.2f  Avg TA: %.2f  Avg Wait: %.2f\n\n", (float) resp / count, (float) ta / count, (float) wait / count);
-}
 
 void RoundRobin(int *arr, int *burst, int count) {
 
---
+++ assn5.h
@@ -0,0 +1,8 @@
+#ifndef assn5
+#define assn5
+
+void SJF(int*, int*, int);
+
+void SRTF(int*, int*, int);
+
+#endif
