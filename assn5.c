b7b1c397aecddc8b14863595d3038e1742082ba7
Kris Olson <36244350+krisolson@users.noreply.github.com>
2018-04-01 23:48:35 -0600

    Add files via upload
    Finally got SRTF working! So this is the entire working file. But not cleaned up to work together...

--- assn5.c
+++ assn5.c
@@ -1,14 +1,5 @@
-/*I promise that this source code file has, in its entirety, been
-written by myself or a member of my group and by no other person or
-persons. If at any time an exact copy of this source code is found to
-be used by another person outside my group in this term, I understand
-that all members of my group and the members of the group that
-submitted the copy will receive a zero on this assignment.
-*/
-//Kris Olson
 #include <stdio.h>
 #include <stdlib.h>
-#include "assn5.h"
 
 #define MAX_PROCS (100)
 #define TIME_Q (100)
@@ -54,6 +45,178 @@ void FCFS(int *arr, int *burst, int count) {
            (float) wait / count);
 }
 
+void SJF(int *arr, int *burst, int count) {
+    int ready[MAX_PROCS], start[MAX_PROCS], end[MAX_PROCS];
+    int clock = 0;
+    int ta = 0, wait = 0, resp = 0;
+    int cur = 0, next = 0, insert = 0, inQ = 0;
+
+    for (int i = 0; i < count; ++i) {
+        if (cur == insert) {
+            ready[insert++] = next;
+            clock = arr[next];
+            ++inQ;
+            ++next;
+        }
+        start[ready[cur]] = clock;
+        clock += burst[ready[cur]];
+        end[ready[cur]] = clock;
+        inQ;
+        ta += end[ready[cur]] - arr[ready[cur]];
+        wait += start[ready[cur]] - arr[ready[cur]];
+        resp += start[ready[cur]] - arr[ready[cur]];
+        while (next < count && arr[next] < clock) {
+            ready[insert++] = next++;
+            ++inQ;
+            for (int i = cur + 1; i < inQ + 1; ++i) {
+                for (int j = i + 1; j < inQ; ++j) {
+                    if (burst[ready[i]] > burst[ready[j]]) {
+                        int temp = ready[i];
+                        ready[i] = ready[j];
+                        ready[j] = temp;
+                    }
+                }
+            }
+        }
+        ++cur;
+    }
+    printf("Shortest Job First\n");
+
+    printf("Avg Resp: %.2f  Avg TA: %.2f  Avg Wait: %.2f\n\n", (float) resp / count, (float) ta / count,
+           (float) wait / count);
+}
+
+//void SRTF(int *arr, int *burst, int count) {
+//    int ready[MAX_PROCS], start[MAX_PROCS], end[MAX_PROCS], remain[MAX_PROCS];
+//    int clock = 0;
+//    int ta = 0, wait = 0, resp = 0;
+//    int next = 0, cur = 0, insert = 0, inQ = 0;
+//
+//    for (int i = 0; i < count; ++i) { //loop to handle all of the count jobs
+//        if (cur == insert) { //no jobs in queue
+//            ready[insert++] = next;  //add next one to ready and inc insert
+//            remain[next] = burst[next];  //set the job in the queue remaining time as full burst
+//            clock = arr[next];  //move the clock to the beginning of the arrivals
+//            start[next] = -1;  //set the start time to -1 so we can record when it actually starts
+//            ++inQ;  //counter for what's in the queue inc
+//            ++next;  //inc next job
+//        }
+//
+//        if (start[ready[cur]] < 0) {start[ready[cur]] = clock;}  //save the first start time
+//
+//        if (clock + remain[ready[cur]] <= arr[next]){ //if job finishes before next arrives
+//			printf("here\n");
+//            end[ready[cur]] = clock + remain[ready[cur]];  //record end of job
+//            start[next] = -1; //put arrived job on queue
+//            ++inQ;  //inc #jobs on queue
+//            remain[next] = burst[next]; //still putting on queue
+//            clock = arr[next]; //moving clock to next job (might be a gap here)
+//        }
+//        else {  //job is running when next arrives this is pausing to see if the new job is shorter
+//            remain[ready[cur]] -= arr[next] - clock;  //subtract the time it ran before new job arrived
+//            clock = arr[next];  //now we're at the new job arrival time
+//
+//            ready[insert++] = next;
+//            ++inQ;
+//            for(int i = cur; i < inQ; ++i) {
+//                for (int j = i + 1; j < inQ-1; ++j){
+//                    if(remain[ready[i]] > remain[ready[j]]) {
+//                        int temp = ready[i];
+//                        ready[i] = ready[j];
+//                        ready[j] = temp;
+//                    }
+//                }
+//            }
+//        }
+//		++next;
+//        ++cur;
+//    }
+//
+//    for(int i = 0; i < count; ++i){
+//        ta += end[i] - arr[i];
+//        wait += end[i] - arr[i] - burst[i];
+//        resp += start[i] - arr[i];
+//    }
+//
+//    printf("Ready Q\n");
+//    for (int k = 0; k < count; ++k) {
+//        printf("%d\n");
+//    }
+//    printf("Shortest Remaining Time First\n");
+//    printf("Avg Resp: %.2f  Avg TA: %.2f  Avg Wait: %.2f\n\n", (float) resp / count, (float) ta / count, (float) wait / count);
+//}
+
+void SRTF(int *arr, int *burst, int count) {
+    int ready[MAX_PROCS], remain[MAX_PROCS], start[MAX_PROCS], end[MAX_PROCS];
+    int clock = 0;
+    int ta = 0, wait = 0, resp = 0;  //turnaround, wait and response time
+    int next = 0, curr = 0, insert = 0, inQ = 0;
+    for (int i = 0; i < 100; ++i) {
+        remain[i] = 0;
+    }
+
+    for (int i = 0; i < 100; ++i) { //still within arrays & not full
+        if (insert <= count) {
+            /*fill empty ready queue*/
+            if (curr == insert) { //no jobs in ready queue, need to put one in
+                ready[insert] = next;  //ready contains pointer to which job
+                remain[next] = burst[next];
+                clock = arr[next];
+                start[next] = -1;
+                next++;
+                insert++;
+                inQ++;
+            }
+            //run the next job in the RQ
+            if (start[ready[curr]] < 0) { start[ready[curr]] = clock; }
+
+            /*if it will have time left*/
+            if (clock + remain[ready[curr]] > arr[next] && next < count) { //if something arrives while it's running
+                remain[ready[curr]] -= arr[next] - clock;//calculate new remaining time
+                clock = arr[next]; //move clock to next job arrival
+            } else { /*if it is done*/
+                clock += remain[ready[curr]]; //move clock to when it is done
+                remain[ready[curr]] -= remain[ready[curr]];  //subtract remaining time
+                end[ready[curr]] = clock; //record end time
+                wait += end[ready[curr]] - burst[ready[curr]] - arr[ready[curr]];
+                ta += end[ready[curr]] - arr[ready[curr]];
+                resp += start[ready[curr]] - arr[ready[curr]];
+                --inQ;
+            }
+
+            /*move arrived bursts to ready queue*/
+            while (next < count && arr[next] <= clock) { //while more have arrived
+                ready[insert] = next;
+                remain[next] = burst[next];
+                start[next] = -1;
+                next++;
+                insert++;
+                inQ++;
+            }
+
+            // find next shortest job
+            int min = 0;
+            for (int i = 0; i < insert; ++i) {
+                if (remain[i] != 0) {
+                    if (min == 0) {
+                        min = i;
+                    } else if (remain[i] < remain[min]) {
+                        min = i;
+                    }
+                }
+                curr = min;
+                if (inQ == 0) {
+                    curr = insert;
+                }
+            }
+        }
+    }
+    printf("Shortest Time Remaining First: \n");
+
+    //calculate stats
+    printf("Avg Resp: %.2f  Avg TA: %.2f  Avg Wait: %.2f\n\n", (float) resp / count, (float) ta / count,
+           (float) wait / count);
+}
 
 void RoundRobin(int *arr, int *burst, int count) {
 
@@ -115,7 +278,8 @@ void RoundRobin(int *arr, int *burst, int count) {
 }
 
 int main(int argc, char *argv[]) {
-    printf("\nAssignment 5 FCFS & Round Robin by Kris Olson\n");
+    printf("\nFCFS & Round Robin by Kris Olson\n");
+    printf("SJF & SRTF by Joseph Jacobson\n");
     printf("Project Team: Kris Olson, Joseph Jacobson\n\n");
 
     FILE *fp;
